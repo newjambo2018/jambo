e1fd0c5d2f6cdce1f75261ba2b8d0c5bb81508e1
mikulenko <nots.a.post@gmail.com>
2018-04-05 20:39:34 +0300

    футер допилю

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -40,15 +40,6 @@
                         </ul>
                     </div>
                 </div>
-                <div class="col-sm-6">
-                    <div class="social-icons pull-right">
-                        <ul class="nav navbar-nav">
-                            <li><a href="#"><i class="fab fa-facebook"></i></a></li>
-                            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                            <li><a href="#"><i class="fab fa-google-plus"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
             </div>
         </div>
     </div><!--/header_top-->
@@ -157,65 +148,29 @@
         <div class="footer-widget">
             <div class="container">
                 <div class="row">
-                    <div class="col-sm-2">
-                        <div class="single-widget">
-                            <h2>Service</h2>
-                            <ul class="nav nav-pills nav-stacked">
-                                <li><a href="#">Online Help</a></li>
-                                <li><a href="#">Contact Us</a></li>
-                                <li><a href="#">Order Status</a></li>
-                                <li><a href="#">Change Location</a></li>
-                                <li><a href="#">FAQ’s</a></li>
-                            </ul>
+                    <div class="col-md-12">
+                        <h3 class="title-name">Информация</h3>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="col-md-2">
+                            Сотрудничество
+                        </div>
+                        <div class="col-md-2">
+                             Контакты магазина
+                        </div>
+                        <div class="col-md-2">
+                            Доставка и оплата
+                        </div>
+                        <div class="col-md-2">
+                            О компании
+                        </div>
+                        <div class="col-md-2">
+                            Сертификаты качества
+                        </div>
+                        <div class="col-md-2">
+                            FAQ
                         </div>
                     </div>
-                    <div class="col-sm-2">
-                        <div class="single-widget">
-                            <h2>Quock Shop</h2>
-                            <ul class="nav nav-pills nav-stacked">
-                                <li><a href="#">T-Shirt</a></li>
-                                <li><a href="#">Mens</a></li>
-                                <li><a href="#">Womens</a></li>
-                                <li><a href="#">Gift Cards</a></li>
-                                <li><a href="#">Shoes</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-sm-2">
-                        <div class="single-widget">
-                            <h2>Policies</h2>
-                            <ul class="nav nav-pills nav-stacked">
-                                <li><a href="#">Terms of Use</a></li>
-                                <li><a href="#">Privecy Policy</a></li>
-                                <li><a href="#">Refund Policy</a></li>
-                                <li><a href="#">Billing System</a></li>
-                                <li><a href="#">Ticket System</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-sm-2">
-                        <div class="single-widget">
-                            <h2>О нас </h2>
-                            <ul class="nav nav-pills nav-stacked">
-                                <li><a href="#">О нас </a></li>
-                                <li><a href="#">Сотрудничество</a></li>
-                                <li><a href="#">Store Location</a></li>
-                                <li><a href="#">Affillate Program</a></li>
-                                <li><a href="#">Copyright</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-sm-3 col-sm-offset-1">
-                        <div class="single-widget">
-                            <h2>Подписаться на наши новости!</h2>
-                            <form action="#" class="searchform">
-                                <input type="text" placeholder="Ваш E-mail адрес"/>
-                                <button type="submit" class="btn btn-default"><i class="fas fa-arrow-right"></i></button>
-                                <p>Получайте самые свежие новости нашей компании, а также свежий каталог товаров...</p>
-                            </form>
-                        </div>
-                    </div>
-
                 </div>
             </div>
         </div>
